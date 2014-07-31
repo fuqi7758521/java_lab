cf31c76602ce15d99f2ffb559333cfe510593b6d
fuqi <fuqi7758521@qq.com>
2014-07-31 10:30:15 +0800

    method demo
    method demo

---
+++ src/com/javalab/MethodDemo.java
@@ -0,0 +1,19 @@
+package com.javalab;
+
+public class MethodDemo {
+	
+	public int max(int a, int b){
+		int max = 0;
+		if(a >= b){
+			max = a;
+		}else{
+			max = b;
+		}
+		return max;
+	}
+	
+	public static void main(String[] args) {
+		MethodDemo demo = new MethodDemo();
+		System.out.println("The max value is: " + demo.max(3, 4));
+	}
+}
